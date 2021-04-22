7a76ab4bca022bef6586c37df24b8e08b4703f38
Hatari <hatari@hatarax.com>
2021-04-22 13:30:34 +0200

    TP1 - Class Lambda

--- tp1/src/MainClassLambda.java
+++ tp1/src/MainClassLambda.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 public class MainClassLambda {
 
     private static ArrayList<UeClass> arrayListUeClass = new ArrayList<>();
-
+    
     public static void main(String[] args) {
+
+        MainClassAnonyme mainClassAnonyme = new MainClassAnonyme();
 
         arrayListUeClass.add(new UeClass("nomModule1", 2, "nomDiplome1", "nomEnseignementResp1", "QCM", false));
         arrayListUeClass.add(new UeClass("nomModule2", 2, "nomDiplome2", "nomEnseignementResp2", "QCM", true));
@@ -17,5 +19,26 @@ public class MainClassLambda {
         arrayListUeClass.add(new UeClass("nomModule9", 2, "nomDiplome9", "nomEnseignementResp9", "questions de synthèse", false));
         arrayListUeClass.add(new UeClass("nomModule10", 2, "nomDiplome10", "nomEnseignementResp10", "Projet", true));
 
+        System.out.println("|------- TRAITEMENT UN --------|");
+        arrayListUeClass.forEach(ueClass -> {
+            ueClass.setAnneeCrea(ueClass.getAnneeCrea() + 1);
+            System.out.println(ueClass.getAnneeCrea());
+        });
+
+        System.out.println("|------- TRAITEMENT DEUX --------|");
+        arrayListUeClass.forEach(ueClass -> {
+            if(ueClass.getNomEnseignementResp().equals("Dupont")) {
+                ueClass.setAnneeCrea(ueClass.getAnneeCrea() + 1);
+                System.out.println(ueClass.getAnneeCrea());
+            }
+        });
+
+        System.out.println("|------- TRAITEMENT TROIS --------|");
+        arrayListUeClass.forEach(ueClass -> {
+            if (ueClass.getTypeControle().equals("QCM") && ueClass.getRattrapage()) {
+                ueClass.setRattrapage(false);
+                System.out.println(ueClass.getRattrapage());
+            }
+        });
     }
 }
