28c62ac6fec67c5f5a94702ee90cfb7f2c7a98b3
Hatari <hatari@hatarax.com>
2021-04-27 13:35:58 +0200

    TP2 - Début TP2

---
+++ tp2/src/AnalyseurDeClasse.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AnalyseurDeClasse {
+
+    public static String litChaineAuClavier() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
+    }
+
+    public static void main(String[] args) {
+        boolean ok = false;
+
+        while(!ok) {
+            try {
+                System.out.print("Entrez le nom d'une classe (ex : java.util.Date): ");
+                String nomClasse = litChaineAuClavier();
+
+                analyseClasse(nomClasse);
+                ok = true;
+            } catch(ClassNotFoundException e) {
+                System.out.println("Classe non trouv�e.");
+            }catch(IOException e) {
+                System.out.println("Erreur d'E/S!");
+            }
+        }
+    }
+
+    public static void analyseClasse(String nomClasse) throws ClassNotFoundException {
+        // R�cup�ration d'un objet de type Class correspondant au nom pass� en param�tres
+        Class cl = // CODE A ECRIRE !
+
+                afficheEnTeteClasse(cl);
+
+        System.out.println();
+        afficheInnerClasses(cl);
+
+        System.out.println();
+        afficheAttributs(cl);
+
+        System.out.println();
+        afficheConstructeurs(cl);
+
+        System.out.println();
+        afficheMethodes(cl);
+
+        // L'accolade fermante de fin de classe !
+        System.out.println("}");
+    }
+
+
+    /** Retourne la classe dont le nom est pass� en param�tre */
+    public static Class getClasse(String nomClasse) throws ClassNotFoundException {
+        // CODE A ECRIRE
+    }
+
+    /** Cette m�thode affiche par ex "public class C1 extends C2 implements I1, I2 {" */
+    public static void afficheEnTeteClasse(Class cl) {/*
+        //  Affichage du modifier et du nom de la classe
+        // CODE A ECRIRE
+
+        // R�cup�ration de la superclasse si elle existe (null si cl est le type Object)
+        Class supercl = // CODE A ECRIRE
+
+                // On ecrit le "extends " que si la superclasse est non nulle et diff�rente de Object
+                // CODE A ECRIRE
+
+                // Affichage des interfaces que la classe implemente
+                // CODE A ECRIRE
+
+                // Accolade ouvrante de d�but de classe
+                System.out.print(" {\n");*/
+    }
+
+    /** Cette m�thode affiche les classes imbriqu�es statiques ou pas
+     A faire apr�s avoir fait fonctionner le reste */
+    public static void afficheInnerClasses(Class cl) {
+        // CODE A ECRIRE
+    }
+
+    public static void afficheAttributs(Class cl) {
+        // CODE A ECRIRE
+    }
+
+    public static void afficheConstructeurs(Class cl) {
+        // CODE A ECRIRE
+        System.out.println("{}");
+
+    }
+
+    public static void afficheMethodes(Class cl) {
+        // CODE A ECRIRE
+        System.out.println("{}");
+    }
+}
+
